c6b08d8d29d81545d400e2728ffde78d96aae440
Captaincool07 <shindeamol181195@gmail.com>
2021-09-25 17:32:10 +0530

    Day5 THA Done

---
+++ Day5/index.js
@@ -0,0 +1,9 @@
+// var fs = require("fs");
+// fs.unlink("day_5/hello.txt");
+// fs.rmdirSync("day_5");
+// fs.writeFileSync("day_5/hello.txt", "Hi Captain");
+
+var test =require("./require");
+
+var obj = new Test();
+console.log(obj.print());
---
+++ Day5/require.js
@@ -0,0 +1,12 @@
+class Test {
+   print(){
+       console.log("I am inisde require");
+   } 
+}
+class Test1{
+    print(){
+        console.log(">>");
+    } 
+}
+
+module.exports = Test;
